8fa264cba840504cc0f83aed9bb6ca3dcac97964
Nikola Jevric <46942531+NJevric@users.noreply.github.com>
2021-02-12 02:24:52 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+    prikaziNavigaciju();
+    active();
+    $(document).scroll(navScroll);
+    prikaziFilter();
+    prikaziGetTicket();
+});
+
+function navScroll(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 80) {
+        $('nav').css({
+            "transition":".5s ease",
+            "padding-bottom": "0px"
+        });
+        $('#logo p').css({
+            'font-size':'1.6em',
+            "transition":".3s ease",
+            "margin-bottom":"10px",
+            "margin-top":"-5px"
+        });
+        
+        $('nav #links').css({
+            'transition':'.3s',
+            'margin-top':'0px'
+        });
+    }
+    else{
+        $('nav').css({
+            "padding-bottom": "15px"
+        });
+        $('#logo p').css({
+            'font-size':'1.8em',
+            "margin":"0px"
+        });
+        
+        $('nav #links').css({
+            "margin":"10px 0px"
+        });
+    }  
+}
+
+function active(){
+    let url = window.location.pathname.slice(1);
+    console.log(url);
+    $('#links').find('a').each(function() {
+      $(this).toggleClass('active', $(this).attr('href') == url);
+   });
+    
+}
+
+function prikaziFilter(){
+    document.querySelector('#pocetniPrikazFilter').addEventListener('click',function(){
+        document.querySelector('#zanrovi').classList.toggle('prikazZanr');
+        
+    })
+}
+
+function prikaziGetTicket(){
+
+    let klasaGet = document.querySelectorAll('.get');
+    console.log(klasaGet);
+
+    for(let i=0; i<klasaGet.length;i++){
+        klasaGet[i].addEventListener('click',function(e){
+            e.preventDefault();
+            console.log(i);
+            document.querySelector('#getTicket').style.display='block';
+        });
+    }
+    document.querySelector('#gornjiGetTicket i').addEventListener('click',function(){
+        document.querySelector('#getTicket').style.display='none';
+    })
+}
+
+function prikaziNavigaciju(){
+   
+    document.querySelector('#hamburger i').addEventListener('click',function(){
+        document.querySelector('#links').classList.toggle('otvoriNav');
+        
+    })
+    
+}
